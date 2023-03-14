fc36e38af51200b3e07b6f5c9bc69aabffc7734f
Alastair D'Silva <alastair@d-silva.org>
2023-03-14 11:10:20 +1100

    Announce steps
    Signed-off-by: Alastair D'Silva <alastair@d-silva.org>

--- setup-auto1111.sh
+++ setup-auto1111.sh
@@ -2,15 +2,19 @@
 
 # Fetch tarball with models and extract
 test -f stable-diffusion-webui.tbz || (
+	echo Downloading tarball
 	pip install gdown
 	gdown 1F9xIZx09ryZF6wDHMJDyA4CN4h1o-WCD
 )
 
-test -d stable-diffusion-webui || \
+test -d stable-diffusion-webui || (
+	echo Extracting tarball
 	tar jxf stable-diffusion-webui.tbz
+)
 
 cd stable-diffusion-webui
 
+echo Launching
 webui.sh --listen --nohalf
 
 
--- setup-textgen.sh
+++ setup-textgen.sh
@@ -2,18 +2,23 @@
 
 # Fetch tarball with models and extract
 test -f text-generation-webui.tar.bz2 || (
+	echo Downloading tarball
 	pip install gdown
 	gdown 1pV2qGujZYs9vqbgabqGCPX14_CK2UzII
 )
 
-test -d text-generation-webui || \
+test -d text-generation-webui || (
+	echo Extracting tarball
 	tar jxf text-generation-webui.tar.bz2
+)
 
 cd text-generation-webui
 
+echo Preparing
 pip install -r requirements.txt
 pip uninstall transformers
 pip install git+https://github.com/zphang/transformers@llama_push
 
+echo Launching
 python server.py --listen
 
